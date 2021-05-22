62402469a5d666fab95908308bd3b978828c4650
TFX0019 <1400kss@gmail.com>
2021-05-21 20:38:06 -0400

    twu jaja

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
   <div class="container">
     <div :class="!side1 ? 'panelOne' : 'panelOne panelOne_active'">
-      <a class="link" v-on:click="toggleTwo">
-        <Icon :width="20" :color="'#fff'" />
-        <span>Este es un link</span>
+      <a v-for="(item, index) in list" :key="index" class="link" v-on:click="toggleTwo(item.hijo.item, index)">
+        <Icon :name="item.icono" :width="20" :color="'#fff'" />
+        <span>{{item.name}}</span>
       </a>
     </div>
-    <div :class="!side2 ? 'panelTwo' : 'panelTwo panelTwo_active'"></div>
+    <div :class="!side2 ? 'panelTwo' : 'panelTwo panelTwo_active'">
+      <a v-for="(item, index) in list2" :key="index" class="link" v-on:click="toggleTwo(item.hijo.item)">
+        <Icon :name="item.icono" :width="20" :color="'#fff'" />
+        <span>{{item.name}}</span>
+      </a>
+    </div>
     <button class="togleButton" v-on:click="toggleOne">Toggle sidebar</button>
   </div>
 </template>
 
 <script>
 import Icon from './components/icon.vue';
+import {menuAdmin} from './sidebar';
+
 export default {
   name: 'App',
   components: {Icon},
   data() {
     return {
       side1: false,
-      side2: false
+      side2: false,
+      list: menuAdmin,
+      list2: [],
+      idmenu2: 0,
     }
+  },
+  mounted() {
+    console.log(this.list)
   },
   methods: {
     toggleOne: function() {
@@ -31,8 +44,9 @@ export default {
         this.side1 = !this.side1;
       }
     },
-    toggleTwo: function() {
-      this.side2 = !this.side2;
+    toggleTwo: function(e) {
+      this.side2 = true;
+      this.list2 = e;
     }
   }
 }
@@ -86,6 +100,9 @@ export default {
   }
 
   .togleButton {
+    position: absolute;
+    top: 20px;
+    right: 20px;
     width: 100px;
     height: 40px;
   }
